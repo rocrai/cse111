db0af9a920ff475b697cb8393be02b0365a10ef6
shukuru <rocrai29@gmail.com>
2023-02-15 17:15:29 +0400

    added things to 07week

---
+++ 07week/random_numbers.py
@@ -0,0 +1,20 @@
+import random
+def main():
+    numbers = [16.2, 75.1, 52.3]
+    print(f'numbers {numbers}')
+    append_random_numbers(numbers)
+    print(f'numbers {numbers}')
+    append_random_numbers(numbers, 3)
+    print(f'numbers {numbers}')
+
+def append_random_numbers(numbers_list, quantity=1):
+    for _ in range(quantity):
+        x = random.uniform(0,100)
+        x = round(x,1)
+        numbers_list.append(x)
+
+# x = random.uniform(0,100)
+# x = round(x,1)
+# print(x)
+if __name__ == '__main__':
+    main()
